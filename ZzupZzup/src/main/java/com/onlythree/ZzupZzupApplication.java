deb4017a439985671d8b486fb413c5bb0a825d1c
jhbae <jhbae.dev@gmail.com>
2021-04-13 14:01:47 +0900

    환경설정

---
+++ ZzupZzup/src/main/java/com/onlythree/ZzupZzupApplication.java
@@ -0,0 +1,13 @@
+package com.onlythree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzupZzupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzupZzupApplication.class, args);
+	}
+
+}
---
+++ ZzupZzup/src/test/java/com/onlythree/ZzupZzupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlythree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzupZzupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
